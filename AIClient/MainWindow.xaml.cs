31ad0673fa13559dffbb51b1d05c7f71f5014fa3
Amiel Cohen <amielc1@gmail.com>
2025-07-24 22:55:06 +0300

    Implement auto-scrolling for message viewer

--- AIClient/MainWindow.xaml.cs
+++ AIClient/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Specialized;
+using AIClient.ViewModels;
 
 namespace AIClient
 {
@@ -10,7 +13,34 @@ namespace AIClient
         public MainWindow()
         {
             InitializeComponent();
-            // DataContext is set in XAML
+
+            // Subscribe to collection changes to auto-scroll
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Messages.CollectionChanged += Messages_CollectionChanged;
+            }
+        }
+
+        private void Messages_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                // Ensure scroll to bottom happens after the UI is updated
+                MessagesScroller.Dispatcher.InvokeAsync(() =>
+                {
+                    MessagesScroller.ScrollToBottom();
+                }, System.Windows.Threading.DispatcherPriority.Loaded);
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            // Clean up the event handler
+            if (DataContext is MainViewModel vm)
+            {
+                vm.Messages.CollectionChanged -= Messages_CollectionChanged;
+            }
+            base.OnClosed(e);
         }
     }
 }
